b57aea8e6f6efade794342eebcdf096075cb530f
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-06-26 17:32:56 +0200

    Create a URL to a post's detail

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ from . import views
     Этот шаблон сообщит Django, что views.post_list это правильное место, если кто-то войдет на ваш сайт по адресу « http://127.0.0.1:8000/ »."""
 urlpatterns = [
     path('', views.post_list, name='post_list'),  # присваиваем view call post_listкорневому URL-адресу.
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
